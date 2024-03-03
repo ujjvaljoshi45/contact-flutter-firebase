9e69be68b7ecfa60a23ed42ba807da60ef0d13ee
Ujjval Joshi <ujjvaljoshi45@gmail.com>
2024-03-03 11:12:17 +0530

    firestore_done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_contacts/firebase_options.dart';
 // import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_contacts/models/contact_provider.dart';
 import 'package:my_contacts/views/home.dart';
@@ -7,6 +10,18 @@ import 'package:provider/provider.dart';
 void main() async {
   //initialize
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).whenComplete(() => print("INIT RUN")).catchError((e) => print("Error:$e"));
+  print("Getting CR");
+  CollectionReference cr = FirebaseFirestore.instance.collection("contacts");
+  String name = "Ujjval";
+  String phno = "007";
+  print("Adding Data");
+  await cr
+      .add({'name': name, 'phno': phno})
+      .then((value) => print("Data Added!"))
+      .catchError((e) => print("Firestore Error: $e"));
 
   runApp(ChangeNotifierProvider(
       create: (context) => ContactModel(), child: const MyApp()));
